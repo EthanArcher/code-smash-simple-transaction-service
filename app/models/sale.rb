776aba59c8c2716f70d7e42c3f669f40542b3c09
Barry Gordon <barry@shopkeep.com>
2014-04-28 18:36:58 +0100

    Add some teaching methods to make errors less cryptic

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -4,4 +4,13 @@ class Sale < ActiveRecord::Base
 
   validates :total, presence: true
   validates :items, presence: true
+
+  # This is purely for teaching purposes, this is normally undesirable
+  def save(*args)
+    save!(*args)
+  end
+
+  def self.create(*args)
+    create!(*args)
+  end
 end
