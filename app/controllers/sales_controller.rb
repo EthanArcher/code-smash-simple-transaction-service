8730c97ef8c9618cc01127a2d1fcbc92dd3ab44c
Ethan Archer <ethanarcher1@gmail.com>
2014-04-30 19:00:11 +0100

    Code for exercise 1 completed

--- app/controllers/sales_controller.rb
+++ app/controllers/sales_controller.rb
@@ -1,15 +1,25 @@
 class SalesController < WebServiceController
 
   def index
-    sales = []
+    sales = Sale.all
     render json: sales
   end
 
   def show
+    render json: Sale.find(params["id"])
     # Hint: look at the 'params' hash
   end
 
   def create
+    sale = Sale.new
+    sale.items = params["sale"]["items"]
+    total = sale.items.collect { |item| item[:price.to_s].to_f }.reduce(:+)
+    sale.total = total
+    sale.valid?
+    sale.save
+    render json: sale
+    
+
     # Hint: check if your newly created sale is valid
     #       by checking sale.valid? and looking at the
     #       errors array
