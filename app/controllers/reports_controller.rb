8a48e9353b5d4d8900ecf2f8ff3615f4d8115f6d
Ethan Archer <ethanarcher1@gmail.com>
2014-05-01 01:59:01 +0100

    All Green Working

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,9 +1,43 @@
 class ReportsController < WebServiceController
 
   def total_sales
+  	sales = Sale.all
+
+  	report = { 'number_of_sales' => 0 , 'total' => 0}
+
+  	sales.each do |sale|
+  		report['number_of_sales'] += 1
+  		report['total'] += sale['total'].to_f
+
+  	end
+
+  	render json: report
+
   end
 
   def sales_per_item
+
+  	finalReport = {}
+  	sales = Sale.all
+  	names = Array.new
+
+  	sales.each do |sale|
+  		sale['items'].each do |item|
+  				name = item['name']
+  				unless finalReport.key? name
+  						finalReport[name] = { 'number_of_sales' => 0 , 'total' => 0 }
+  				end
+  				finalReport[name]['number_of_sales'] +=1 
+  				finalReport[name]['total'] += item['price']
+
+  				if item.key? 'discount'
+  					finalReport[name]['total'] -= item['discount']['amount'].to_f
+  				end
+
+  		end
+  	end
+
+  	render json: finalReport
   end
 
 end
--- app/controllers/sales_controller.rb
+++ app/controllers/sales_controller.rb
@@ -6,20 +6,27 @@ class SalesController < WebServiceController
   end
 
   def show
-    render json: Sale.find(params["id"])
+    render json: Sale.find(params['id'])
     # Hint: look at the 'params' hash
   end
 
   def create
+    
     sale = Sale.new
-    sale.items = params["sale"]["items"]
-    total = sale.items.collect { |item| item[:price.to_s].to_f }.reduce(:+)
-    sale.total = total
+    sale.items = params['sale']['items']
+    sale['total'] = 0
+
+    sale['items'].each do |item|
+      sale['total'] += item['price'].to_f
+
+      if item.key? 'discount'
+        sale['total'] -= item['discount']['amount'].to_f
+      end
+    end
+
     sale.valid?
     sale.save
     render json: sale
-    
-
     # Hint: check if your newly created sale is valid
     #       by checking sale.valid? and looking at the
     #       errors array
