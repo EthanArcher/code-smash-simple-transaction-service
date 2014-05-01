d1bc36688172bcee0dc2d0bd9c45a0b85e472a72
Ethan Archer <ethanarcher1@gmail.com>
2014-05-01 02:14:02 +0100

    code commented

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,43 +1,42 @@
 class ReportsController < WebServiceController
 
   def total_sales
-  	sales = Sale.all
+  	sales = Sale.all #sales set to all sales
 
-  	report = { 'number_of_sales' => 0 , 'total' => 0}
+  	report = { 'number_of_sales' => 0 , 'total' => 0} #report contails number of sales and total both initiased as zero
 
-  	sales.each do |sale|
-  		report['number_of_sales'] += 1
-  		report['total'] += sale['total'].to_f
+  	sales.each do |sale| 	#for each sale
+  		report['number_of_sales'] += 1 	#increment the number of sales by 1
+  		report['total'] += sale['total'].to_f #increase the total by the total price of that sale
 
   	end
 
-  	render json: report
+  	render json: report #render the report
 
   end
 
   def sales_per_item
 
-  	finalReport = {}
-  	sales = Sale.all
-  	names = Array.new
+  	finalReport = {} #initalise the finalReport empty
+  	sales = Sale.all #sales set to all sales
 
-  	sales.each do |sale|
-  		sale['items'].each do |item|
-  				name = item['name']
-  				unless finalReport.key? name
-  						finalReport[name] = { 'number_of_sales' => 0 , 'total' => 0 }
+  	sales.each do |sale| #for each sale
+  		sale['items'].each do |item| #for each item of each sale
+  				name = item['name'] #variable name holds the name of the item
+  				unless finalReport.key? name #if the name is not already included
+  						finalReport[name] = { 'number_of_sales' => 0 , 'total' => 0 } #create the item name and set number of sales to zero and total to zero
   				end
-  				finalReport[name]['number_of_sales'] +=1 
-  				finalReport[name]['total'] += item['price']
+  				finalReport[name]['number_of_sales'] +=1 #increment the sales of the item with that name
+  				finalReport[name]['total'] += item['price'] #increment the total by the price of that item
 
-  				if item.key? 'discount'
-  					finalReport[name]['total'] -= item['discount']['amount'].to_f
+  				if item.key? 'discount'	#check if the item has been discounted
+  					finalReport[name]['total'] -= item['discount']['amount'].to_f #if so reduce the price by the amount of the discount
   				end
-
   		end
   	end
 
-  	render json: finalReport
+  	render json: finalReport #render the finalReport
+
   end
 
 end
--- app/controllers/sales_controller.rb
+++ app/controllers/sales_controller.rb
@@ -1,35 +1,39 @@
 class SalesController < WebServiceController
 
   def index
-    sales = Sale.all
-    render json: sales
+    sales = Sale.all?  #sales set to contain all sales
+    render json: sales #render sales
+    
   end
 
   def show
-    render json: Sale.find(params['id'])
+    render json: Sale.find(params['id']) # sale can be found using its id parameter
     # Hint: look at the 'params' hash
+    
   end
 
   def create
     
-    sale = Sale.new
-    sale.items = params['sale']['items']
-    sale['total'] = 0
+    sale = Sale.new   #new sale created 
+    sale.items = params['sale']['items'] #item parameters set
+    sale['total'] = 0 #total set to zero
+    
 
-    sale['items'].each do |item|
-      sale['total'] += item['price'].to_f
+    sale['items'].each do |item|  #for each sale item
+      sale['total'] += item['price'].to_f #total = total + price of item
 
-      if item.key? 'discount'
-        sale['total'] -= item['discount']['amount'].to_f
+      if item.key? 'discount' #if the key cointains discount the item is discounted
+        sale['total'] -= item['discount']['amount'].to_f #therefore reduce total by the discounted amount
       end
     end
 
-    sale.valid?
-    sale.save
-    render json: sale
+    sale.valid? #check if the sale is valid
+    sale.save #save the sale
+    render json: sale #render the sale
+
     # Hint: check if your newly created sale is valid
     #       by checking sale.valid? and looking at the
     #       errors array
-  end
 
+  end
 end
